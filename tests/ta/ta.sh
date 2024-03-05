4d9532881c3c5e325f31f6d0d270479c3d9e63b7
Albert van Veldhuizen <albert@xel.nl>
2024-03-05 17:19:48 +0100

    Added wp-env

--- tests/ta/ta.sh
+++ tests/ta/ta.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-wp-env stop && wp-env start &&
+yarn global add @wordpress/scripts @wordpress/env copy-webpack-plugin
+node_modules/.bin/wp-env stop && node_modules/.bin/wp-env start &&
 ## Copy script from root dir to the test dir
 cp ../../WPInstallScript.php ./
 cp ../../.htaccess ./
@@ -9,8 +10,8 @@ cp ../../.htaccess ./
 scriptContent=$(<WPInstallScript.php)
 htaccessContent=$(<.htaccess)
 # Use wp-env run tests-cli to execute a command inside the container to copy the content to another file
-echo "$scriptContent" | wp-env run tests-cli tee /var/www/html/WPInstallScript.php > /dev/null
-echo "$htaccessContent" | wp-env run tests-cli tee /var/www/html/.htaccess > /dev/null
+echo "$scriptContent" | node_modules/.bin/wp-env run tests-cli tee /var/www/html/WPInstallScript.php > /dev/null
+echo "$htaccessContent" | node_modules/.bin/wp-env run tests-cli tee /var/www/html/.htaccess > /dev/null
 
 # Get db config from the docker container using theses commands
 db_name=$(wp-env run tests-cli wp config get DB_NAME)
@@ -24,11 +25,11 @@ echo "DB_USER=$db_user" >> .db.env
 echo "DB_PASS=$db_password" >> .db.env
 echo "DB_HOST=$db_host" >> .db.env
 
-cat .db.env | wp-env run tests-cli tee /var/www/html/.db.env > /dev/null
+cat .db.env | node_modules/.bin/wp-env run tests-cli tee /var/www/html/.db.env > /dev/null
 
 cleanupContent=$(<cleanup.sh)
 echo "$cleanupContent" | wp-env run tests-cli tee /var/www/html/cleanup.sh > /dev/null
-wp-env run tests-cli chmod +x cleanup.sh
-wp-env run tests-cli ./cleanup.sh
-wp-env run tests-cli php WPInstallScript.php -p p -d d
+node_modules/.bin/wp-env run tests-cli chmod +x cleanup.sh
+node_modules/.bin/wp-env run tests-cli ./cleanup.sh
+node_modules/.bin/wp-env run tests-cli php WPInstallScript.php -p p -d d
 php test.php
