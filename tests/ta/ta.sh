fb0c3bb34af222e3ba6a8eeb6d09f291b876b167
Albert van Veldhuizen <albert@xel.nl>
2024-03-06 16:26:04 +0100

    Update tests/ta/ta.sh

--- tests/ta/ta.sh
+++ tests/ta/ta.sh
@@ -23,7 +23,7 @@ echo "DB_HOST=$db_host" >> .db.env
 
 cat .db.env | node_modules/.bin/wp-env run tests-cli tee /var/www/html/.db.env > /dev/null
 
-cleanupContent=$(<cleanup.sh)
+cleanupContent=$(<./cleanup.sh)
 echo "$cleanupContent" | wp-env run tests-cli tee /var/www/html/cleanup.sh > /dev/null
 node_modules/.bin/wp-env run tests-cli chmod +x cleanup.sh
 node_modules/.bin/wp-env run tests-cli ./cleanup.sh
