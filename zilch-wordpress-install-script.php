4cedb0c653c206c6ee7e3ae4922f025aabf0f9ea
nizar <nizar@liberta.nl>
2024-03-08 14:18:47 +0100

    fix test login form

--- zilch-wordpress-install-script.php
+++ zilch-wordpress-install-script.php
@@ -8,7 +8,12 @@ class ScriptHelper {
 
     private const wordpressVersion = '6.4.2';
 
-    private const graphqlPluginVersion = "0.4.1";
+    private const graphqlGutenbergPluginVersion = "0.4.1";
+
+    private const graphqlPluginVersion = "1.22.0";
+
+    private const contactForm7Version = "5.9";
+
 
     public function __construct($wordpressPath, $envFilePath, $runlevel)
     {
@@ -20,22 +25,30 @@ class ScriptHelper {
     /**
      * downloads a wp-cli.phar files that will help executing wordpress commands
      * @return void
+     * @throws Exception
      */
     private function downloadPharFile(): void
     {
-        $downloadUrl = "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar";
-        $directory = __DIR__ . "/WPResources";
-        // If directory doesnt exists, create one!
-        if (!file_exists($directory)) {
-            mkdir($directory, 0777, true);
-        }
-        $filePath = $directory . "/wp-cli.phar";
-        if (file_put_contents($filePath, file_get_contents($downloadUrl)) !== false) {
-            chmod($filePath, 0755);
+        try {
+            $downloadUrl = "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar";
+            $directory = __DIR__ . "/WPResources";
+            // If directory doesnt exists, create one!
+            if (!file_exists($directory)) {
+                mkdir($directory, 0777, true);
+            }
+            $filePath = $directory . "/wp-cli.phar";
+            if (file_put_contents($filePath, file_get_contents($downloadUrl)) !== false) {
+                chmod($filePath, 0755);
+            }
+        } catch (Error|Exception|Throwable $e) {
+            throw new Exception("Something went wrong while downloading wp phar file", 500, $e->getMessage());
         }
     }
 
-   // Checks if the wordpress dir exists
+    /**
+     * Checks if the wordpress dir exists
+     * @return bool
+     */
     private function wordpressDirExists(): bool {
         return file_exists($this->wordpressPath);
     }
@@ -49,7 +62,9 @@ class ScriptHelper {
     private function removeFile($path): void {
         if(file_exists($path)) {
             unlink($path);
+            return;
         }
+        echo "File at the path: $path doesnt exist";
     }
 
     /**
@@ -78,7 +93,9 @@ class ScriptHelper {
      * @throws Exception
      */
     private function executeCoreDownload(): void {
-        exec('php ' . __DIR__ . '/WPResources/wp-cli.phar core download --version=' . escapeshellarg(self::wordpressVersion) . ' --path=' . $this->wordpressPath);
+        if (!exec('php ' . __DIR__ . '/WPResources/wp-cli.phar core download --version=' . escapeshellarg(self::wordpressVersion) . ' --path=' . $this->wordpressPath)) {
+            throw new Exception("The wordpress core was not downloaded successfully", 500);
+        }
         if (!$this->wordpressDirExists()) {
             throw new Exception("The wordpress core was not downloaded successfully", 500);
         }
@@ -103,10 +120,13 @@ class ScriptHelper {
     /**
      * Reads the env file vars and uses it to execute a config create command at the wordpress directory
      * @return void
+     * @throws Exception
      */
     private function executeCreateWpConfig(): void {
         $envData = $this->readEnvFile();
-        exec('php ' . __DIR__ . '/WPResources/wp-cli.phar config create --dbname=' . escapeshellarg($envData["DB_NAME"]) . ' --dbuser=' . escapeshellarg($envData["DB_USER"]) . ' --dbpass=' . escapeshellarg($envData["DB_PASS"]) . ' --dbhost='. escapeshellarg($envData["DB_HOST"] ?? "localhost") .' --path=' . $this->wordpressPath);
+        if(!exec('php ' . __DIR__ . '/WPResources/wp-cli.phar config create --dbname=' . escapeshellarg($envData["DB_NAME"]) . ' --dbuser=' . escapeshellarg($envData["DB_USER"]) . ' --dbpass=' . escapeshellarg($envData["DB_PASS"]) . ' --dbhost='. escapeshellarg($envData["DB_HOST"] ?? "localhost") .' --path=' . $this->wordpressPath)) {
+            throw new Exception("Something went wrong while creating wordpress database config", 500);
+        }
     }
 
     /**
@@ -115,11 +135,13 @@ class ScriptHelper {
      * @param $domainName
      * @param $projectName
      * @return void
+     * @throws Exception
      */
     private function executeWpCoreInstall($domainName, $projectName): void {
-        $adminPassword = "password";
-        $adminEmail = "ta@zilch.nl";
-        exec('php ' . __DIR__ . '/WPResources/wp-cli.phar core install --url=' . escapeshellarg($domainName) . ' --title=' . escapeshellarg($projectName) . ' --admin_user=zilch-admin --admin_password=' . escapeshellarg($adminPassword) . ' --admin_email=' . escapeshellarg($adminEmail) . ' --path=' . $this->wordpressPath);
+        $adminEmail = "email@zilch.nl";
+        if(!exec('php ' . __DIR__ . '/WPResources/wp-cli.phar core install --url=' . escapeshellarg($domainName) . ' --title=' . escapeshellarg($projectName) . ' --admin_user=zilch-admin ' . '--admin_email=' . escapeshellarg($adminEmail) . ' --path=' . $this->wordpressPath)) {
+            throw new Exception("Something went wrong while installing wordpress core for the given domain name: $domainName", 500);
+        }
     }
 
     /**
@@ -128,20 +150,23 @@ class ScriptHelper {
      */
     private function executeWpLanguageCommands(): void {
         try {
-            if(!exec('php ' . __DIR__ . '/WPResources/wp-cli.phar --path=' . escapeshellarg($this->wordpressPath) . ' language core install nl_NL')) {
-                throw new Exception("Error executing language core install");
+            if(!exec('php ' . __DIR__ . '/WPResources/wp-cli.phar --path=' . escapeshellarg($this->wordpressPath) . ' language core install nl_NL --activate')) {
+                throw new Exception("Error executing language core install", 500);
             }
-            exec('php ' . __DIR__ . '/WPResources/wp-cli.phar --path=' . escapeshellarg($this->wordpressPath) . ' language core update');
-        } catch (\Throwable $t) {
+        } catch (\Throwable|Exception|Error $t) {
             throw new Exception("Something went wrong while installing and updating the language", 500, $t->getPrevious());
         }
     }
 
     /**
      * Execute an install plugin command on the wordpress directory
+     * @param String $plugin
+     * @throws Exception
      */
-    private function installPlugin($plugin): void {
-        exec('php ' . __DIR__ . '/WPResources/wp-cli.phar plugin install ' . escapeshellarg($plugin) . ' --activate --path=' . escapeshellarg($this->wordpressPath));
+    private function installPlugin(String $plugin): void {
+        if(!exec('php ' . __DIR__ . '/WPResources/wp-cli.phar plugin install ' . escapeshellarg($plugin) . ' --activate --path=' . escapeshellarg($this->wordpressPath))) {
+            throw new Exception("Something went wrong while installing the plugin: $plugin", 500);
+        }
     }
 
     /**
@@ -164,11 +189,13 @@ class ScriptHelper {
      * @throws Exception
      */
     private function installPlugins(): void {
-        $wpGraphQlGutenbergVersion = self::graphqlPluginVersion;
+        $wpGraphQlGutenbergVersion = self::graphqlGutenbergPluginVersion;
+        $wpGraphQLVersion = self::graphqlPluginVersion;
+        $contactForm7Version = self::contactForm7Version;
         $plugins = [
-            'wp-graphql' => 'wp-graphql',
+            'wp-graphql' => "https://downloads.wordpress.org/plugin/wp-graphql.$wpGraphQLVersion.zip",
             'wp-graphql-gutenberg' => "https://github.com/pristas-peter/wp-graphql-gutenberg/archive/refs/tags/v$wpGraphQlGutenbergVersion.zip",
-            'contact-form-7' => 'contact-form-7',
+            'contact-form-7' => "https://downloads.wordpress.org/plugin/contact-form-7.$contactForm7Version.zip",
             'zilch-assistant-plugin' => 'https://gitlab.xel.nl/albert/kameleon-assistant-plugin-zip/-/raw/latest/zilch-assistant-plugin.zip'
         ];
         foreach ($plugins as $pluginName => $pluginSlug) {
@@ -196,11 +223,24 @@ class ScriptHelper {
         }
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data) . "\n";
+        if($error) {
+            exit($error->getCode());
+        }
     }
 
     private function isProduction(): bool {
         $lowercaseRunLevel = strtolower($this->runlevel);
         return $lowercaseRunLevel === "prod" || $lowercaseRunLevel === "production";
+    }
+
+    private function cleanUpScript($removeWordPress = false): void {
+        $this->removeFile($this->envFilePath);
+        $this->removeDir(__DIR__."/WPResources");
+        unlink(__FILE__);
+        if($removeWordPress) {
+            $this->removeDir($this->wordpressPath);
+            $this->removeFile(__DIR__."/.htaccess");
+        }
     }
 
     /**
@@ -213,13 +253,11 @@ class ScriptHelper {
      * @param $projectName
      * @return void
      */
-    public function installWpScripts($domainName, $projectName): void
-    {
+    public function installWpScripts($domainName, $projectName, $forceInstall = false): void {
         try {
             if($this->wordpressDirExists()) {
                 $error = new Error("The cms directory already exists", 400);
                 $this->generateResponse($error);
-                exit($error->getCode());
             }
             $this->downloadPharFile();
             $this->executeCoreDownload();
@@ -228,32 +266,39 @@ class ScriptHelper {
             $this->executeWpLanguageCommands();
             $this->installPlugins();
             $this->generateResponse();
+            $this->cleanUpScript();
         } catch (Error|Exception|Throwable $e) {
-            if($this->isProduction()) {
-                $this->removeDir($this->wordpressPath);
-                $this->removeFile($this->envFilePath);
-                $this->removeDir(__DIR__."/WPResources");
-                unlink(__FILE__);
-            }
+            $this->cleanUpScript(true);
             $this->generateResponse($e);
-            exit($e->getCode());
         }
     }
 }
 
-// get the options of the command
-$options = getopt("p:d:r:", ["projectName:", "domainName:", "RUN_LEVEL"]);
 
-// if the short_options/long_options don't exist it will fail and prints how to use the command properly
-if ((!isset($options['p']) || !isset($options['d'])) &&
-    (!isset($options['projectName']) || !isset($options['domainName']))) {
-    echo "Usage: php WPInstallScript.php -p <projectName> -d <domainName> OR php WPInstallScript.php --projectName=<projectName> --domainName=<domainName>\n";
-    exit(1);
+function getOptions() {
+    // Check if the script is called from the command line or web browser
+    if (php_sapi_name() === 'cli') {
+        // Get options from the command line
+        return getopt("p:d:r:", ["projectName:", "domainName:", "RUN_LEVEL"]);
+    } else {
+        // Get options from query parameters in the URL
+        return [
+            'p' => $_GET['projectName'] ?? null,
+            'd' => $_GET['domainName'] ?? null,
+        ];
+    }
 }
 
-$projectName = $options['p'] ?? $options['projectName'];
-$domainName = $options['d'] ?? $options['domainName'];
-$wordpressPath = __DIR__."/cms";
+// get the options of the command
+$options = getOptions();
+// get the project name and domain name from the short/long options
+$projectName = $options['p'] ?? $options['projectName'] ?? null;
+$domainName = $options['d'] ?? $options['domainName'] ?? null;
+if(!$domainName || !$projectName) {
+    echo "Usage: php zilch-wordpress-install-script.php -p <projectName> -d <domainName> OR php zilch-wordpress-install-script.php --projectName=<projectName> --domainName=<domainName>";
+    exit(1);
+}
+// set run level by default to development if it was not given
 $runLevel = $options['r'] ?? $options["RUN_LEVEL"] ?? "development";
-$helper = new ScriptHelper($wordpressPath, __DIR__."/.db.env", $runLevel);
+$helper = new ScriptHelper(__DIR__."/cms", __DIR__."/.db.env", $runLevel);
 $helper->installWpScripts($domainName, $projectName);
