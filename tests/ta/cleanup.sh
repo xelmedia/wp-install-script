c78dce468638513bbd50cf2fd3d3591c1946e3ad
Nizar <nizar@liberta.nl>
2024-03-08 14:57:34 +0100

    Dev

--- tests/ta/cleanup.sh
+++ tests/ta/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . -type f ! -name 'WPInstallScript.php' ! -name '.db.env' ! -name '.htaccess' -exec rm {} +
+find . -type f ! -name 'zilch-wordpress-install-script.php' ! -name '.db.env' ! -name '.htaccess' -exec rm {} +
 find . -mindepth 1 -type d -empty -delete
